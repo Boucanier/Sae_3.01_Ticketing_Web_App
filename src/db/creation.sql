c055d9148f1ac86bfc4902787c69128964b27222
boucanier <chironjules.78@gmail.com>
2023-11-10 11:38:14 +0100

    Ajout des utilisateurs par défaut

--- src/db/creation.sql
+++ src/db/creation.sql
@@ -67,3 +67,7 @@ BEGIN
     END IF;
 END;//
 delimiter ;
+
+INSERT INTO Users VALUES ('admin', 'admin', 'admin', 'd033e22ae348aeb5660fc2140aec35850c4da997', 'sys_admin');
+INSERT INTO Users VALUES ('tec1', 'tec1', 'tec1', '73f7a2f5b9bd744ab54cd1d307975868fc93a844', 'tech');
+INSERT INTO Users VALUES ('tec2', 'tec2', 'tec2', '73f7a2f5b9bd744ab54cd1d307975868fc93a844', 'tech');
