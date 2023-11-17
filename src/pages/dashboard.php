65197d2795a60d5617eeba8fe887d974f251ce80
Thomas GODINEAU <thomasgodineau2004@gmail.com>
2023-11-17 10:45:10 +0100

    Dashboard user

--- src/pages/dashboard.php
+++ src/pages/dashboard.php
@@ -1,69 +1,3 @@
-<!--
-
-<!DOCTYPE html>
-<html lang="fr">
-<head>
-    <meta charset="UTF-8">
-    <title>Tableau de bord</title>
-    <link rel="stylesheet" type="text/css" href="../style/style.css">
-</head>
-<body>
-<header>
-    <div id="top">
-        <img src="../resources/logo.png" alt="logo de la plateforme" id="image1">
-        <h1>Ticket App</h1>
-        <img src="../resources/logo_UVSQ.png" alt="logo de l'UVSQ" id="image2">
-    </div>
-    <nav>
-        <div id="nav1">
-            <a href="index.php">Accueil</a>
-            <a href="dashboard.php">Tableau de bord</a>
-        </div>
-        <div id="nav2">
-            <a href="profile.php">Profil</a>
-            <a href="connection.php">Se connecter</a>
-            <a href="index.php">Déconnexion</a>
-        </div>
-    </nav>
-</header>
-<main>
-    <div id="part_top">
-    <h2>Mes tickets</h2>    
-        <button type="button" onclick="location.href='ticket.php'">Créer un ticket</button>
-    </div>
-    <div id="ticket_table">
-    <table>
-        <tr>
-            <th>Niveau</th>
-            <th>Salle</th>
-            <th>Problème</th>
-            <th>Date</th>
-            <th>État</th>
-        </tr>
-        <tr>
-            <td class="ticket_case_1">1</td>
-            <td>315</td>
-            <td>Câble projecteur HS</td>
-            <td>04/10/2023</td>
-            <td>En cours</td>
-        </tr>
-        <tr>
-            <td class="ticket_case_4">4</td>
-            <td>G23</td>
-            <td>Écran cassé</td>
-            <td>14/09/2023</td>
-            <td>Fermé</td>
-        </tr>
-    </table>
-    <div id="details_button">
-        <button type="button" onclick="location.href='ticket_details.php'">Détails</button>
-        <button type="button" onclick="location.href='ticket_details.php'">Détails</button>
-    </div>
-    </div>
-</main>
-
--->
-
 <?php
     include "header.php";
 
@@ -76,42 +10,86 @@
     $mdp = "ticket_s301";
     $nom_db = "ticket_app";
 
+    /*
     $db = mysqli_connect($host, $login, $mdp) or die("Can't connect to database");
 
-    mysqli_select_db($db, $nom_db) or die("Can't open the databade");
+    mysqli_select_db($db, $nom_db) or die("Can't open the database");
+    */
 
     echo '
-    <div id="part_top">
-    <h2>Mes tickets</h2>    
-        <button type="button" onclick="location.href=\'ticket.php\'">Créer un ticket</button>
-    </div>
+        <div id="part_top">
+        <h2>Mes tickets</h2>    
+            <button type="button" onclick="location.href=\'ticket.php\'">Créer un ticket</button>
+        </div>
     ';
 
-    $requete_mes_tickets = "SELECT emergency, room, description, creation_date, status
-                            FROM tickets
-                            WHERE USER_LOGIN = $login;";
-
-    $mes_tickets = mysqli_query($db, $requete_mes_tickets);
 
     echo '
-    <div id="ticket_table">
-    <table>
-    <tr>
+        <div id="ticket_table">
+        <table>
+        <tr>
     ';
 
-    foreach ( as $elem){
-        echo '<th>$elem</th>
+    /*
+    ### Partie pour ajouter les colonnes au tableau ###
+
+    $requete_colonnes = 'SHOW COLUMNS
+                         FROM tickets;';
+
+    $colonnes = mysqli_query($db, $requete_colonnes);
+
+    $colonnes_voulues = array('emergency', 'room', 'title', 'creation_date', 'status');
+
+    for (mysqli_fetch_array($colonnes) as $colonne){
+        if ($colonne in $colonnes_voulues)
+            echo '<th>$colonne</th>';
+    '
+    }
+
+    echo '<tr>';
+
+    ### Partie pour ajouter les tickets au tableau ###
+
+    $requete_tickets = 'SELECT emergency, room, title, creation_date, status
+                        FROM tickets
+                        WHERE user_login = $login;';
+
+    # ATTENTION $login ci-dessus devra être remplacé par les cookies de session
+
+    $tickets = mysqli_query($db, $requete_tickets);
+
+    foreach (mysqli_fetch_array($tickets) as $elem){
+        echo '
+        <tr>
+            <td>$elem[0]</td>
+            <td>$elem[1]</td>
+            <td>$elem[2]</td>
+            <td>$elem[3]</td>
+            <td>$elem[4]</td>
+        </tr>
         ';
-}
-
+    }
+    */
 
     echo '
-    </table>
-    <div id="details_button">
-        <button type="button" onclick="location.href=\'ticket_details.php\'">Détails</button>
-        <button type="button" onclick="location.href=\'ticket_details.php\'">Détails</button>
-    </div>
-    </div>
+        </table>
+        <div id="details_button">
+    ';
+
+    /*
+    $requete_nb_tickets = 'SELECT COUNT(ticket_id)
+                           FROM tickets
+                           WHERE user_login = $login;';
+
+    $nb_tickets = mysqli_query($db, $requete_nb_tickets);
+
+    for (int i=0; i<$nb_tickets; i++){
+            echo '<button type="button" onclick="location.href=\'ticket_details.php\'">Détails</button>';
+    }
+    */
+    echo'
+        </div>
+        </div>
     ';
 
     include "footer.php";
