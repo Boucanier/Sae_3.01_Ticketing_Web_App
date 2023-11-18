0f5b6513f53299285a6b2cd708c59c208894e156
boucanier <chironjules.78@gmail.com>
2023-11-18 15:03:19 +0100

    Préparation de la page pour les requêtes SQL et changement des boutons en fonction de l'utilisation de la page

--- src/pages/ticket_details.php
+++ src/pages/ticket_details.php
@@ -25,22 +25,57 @@
     </nav>
 </header>
 <main>
-    <div id="part_top">
-        <h2>Ticket du jj/mm/aaa</h2>
+    <?php
+    
+    # TODO: Remplacer ce tableau par une requête SQL
+    $data = array('jj/mm/aaaa',
+            'Bacon ipsum dolor amet shoulder short ribs burgdoggen, picanha pancetta chicken pastrami t-bone cow beef buffalo landjaeger. Meatball boudin tenderloin pork belly, chuck pork bacon. Ham hock t-bone bacon turkey. Chislic picanha buffalo, bresaola prosciutto venison tail pig porchetta spare ribs kielbasa short loin beef ribs capicola. Shoulder tail pastrami ground round brisket tri-tip burgdoggen kevin short loin jowl alcatra.',
+            'Libellé',
+            'G24',
+            '3',
+            'in_progress');
+
+    echo '<div id="part_top">
+        <h2>Ticket du '.$data[0].'</h2>
     </div>
-    <div id="ticket_about">
+    <form id="ticket_about">
         <div id="ticket_description">
             <h3>Description du problème</h3>
-            <p>Bacon ipsum dolor amet shoulder short ribs burgdoggen, picanha pancetta chicken pastrami t-bone cow beef buffalo landjaeger. Meatball boudin tenderloin pork belly, chuck pork bacon. Ham hock t-bone bacon turkey. Chislic picanha buffalo, bresaola prosciutto venison tail pig porchetta spare ribs kielbasa short loin beef ribs capicola. Shoulder tail pastrami ground round brisket tri-tip burgdoggen kevin short loin jowl alcatra.
-                Flank tail meatball sausage drumstick. Swine pig bacon, venison cupim tenderloin short ribs shank meatball jowl spare ribs ground round. Landjaeger chicken corned beef capicola. Doner pork belly buffalo, kevin porchetta pancetta filet mignon meatloaf. Hamburger andouille landjaeger jerky, buffalo fatback pork belly frankfurter beef drumstick bacon brisket. Salami shoulder ham hock pork loin short loin.</p>
+            <p>'.$data[1].'</p>
         </div>
         <div id="ticket_details">
-            <p>Libellé</p>
-            <p>Salle : G24</p>
-            <p>Niveau d'urgence : 3</p>
-            <p>État : Fermé</p>
+            <p>'.$data[2].'</p>
+            <p>Salle : '.$data[3].'</p>
+            <p>Niveau d\'urgence : '.$data[4].'</p>';
+
+            switch ($data[5]){
+                case 'open':
+                    echo '<p>État : Ouvert</p>';
+                    break;
+                case 'in_progress':
+                    echo '<p>État : En cours</p>';
+                    break;
+                case 'closed':
+                    echo '<p>État : Fermé</p>';
+                    break;
+                default:
+                    echo '<p>État : Inconnu</p>';
+            }
+
+            echo '<br>
+            <div class="resetSubmitButtons">
+                <input type="button" value="Annuler" class="reset_buttons" onclick="history.back();">';
+                    if ($_GET['function'] == 'take'){
+                        echo '<input type="submit" value="Prendre en charge" name="take" class="submit_buttons">';
+                    }
+                    
+                    else if ($_GET['function'] == 'close'){
+                        echo '<input type="submit" value="Clore" name="close" class="submit_buttons">';
+                    }
+                ?>
+            </div>
         </div>
-    </div>
+    </form>
 </main>
 <?php
     include "footer.php";
