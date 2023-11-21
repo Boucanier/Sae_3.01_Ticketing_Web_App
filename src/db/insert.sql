1d40b449b0732e153aadef2a2574c346ae4ab06b
Thomas GODINEAU <thomasgodineau2004@gmail.com>
2023-11-21 15:10:33 +0100

    liste d_inserts pour tests

--- src/db/insert.sql
+++ src/db/insert.sql
@@ -1,25 +1,25 @@
 INSERT INTO Users VALUES ('user1', 'user1', 'user1', '12dea96fec20593566ab75692c9949596833adc9', 'user');
-INSERT INTO Users VALUES ('user2', 'user1', 'user1', '12dea96fec20593566ab75692c9949596833adc9', 'user');
-INSERT INTO Users VALUES ('user3', 'user1', 'user1', '12dea96fec20593566ab75692c9949596833adc9', 'user');
+INSERT INTO Users VALUES ('user2', 'user2', 'user2', '12dea96fec20593566ab75692c9949596833adc9', 'user');
+INSERT INTO Users VALUES ('user3', 'user3', 'user3', '12dea96fec20593566ab75692c9949596833adc9', 'user');
 /*
     mpd = user
 */
 
 INSERT INTO TICKETS VALUES (1, 'Il pleut sur les PC', 'Il pleut sur les PC lol', 'G26', 'open', 3, '2023-11-20', 'user3', '192.168.0.1');
 INSERT INTO TICKETS VALUES (2, 'La B fait trop de bruit', 'La B fait trop de bruit lol', 'G21', 'closed', 4, '2023-10-15', 'user1', '192.168.0.2');
-INSERT INTO TICKETS VALUES (3, 'Les PC sont lents', 'Les PC sont lents lol', 'I21', 'in_progress', 1, '2023-05-20', 'user2', '192.168.0.1');
-INSERT INTO TICKETS VALUES (4, 'La souris marche pas', 'La souris marche pas lol', 'G22', 'in_progress', 2, '2023-08-23', 'user2', '192.168.0.2');
-INSERT INTO TICKETS VALUES (5, 'Il manque des touches', 'Il manque des touche lol', 'G26', 'open', 1, '2023-10-09', 'user1', '192.168.0.3');
-INSERT INTO TICKETS VALUES (6, 'Le projecteur marche pas', 'Le projecteur marche pas lol', 'G25', 'closed', 2, '2022-05-20', 'user3', '192.168.0.3');
-INSERT INTO TICKETS VALUES (7, 'L ecran est pété', 'L ecran est pété lol', 'G23', 'in_progress', 4, '2023-12-15', 'user2', '192.168.0.2');
-INSERT INTO TICKETS VALUES (8, 'J ai plus d idees', 'J ai plus d idees lol', 'G24', 'closed', 4, '2023-10-20', 'user1', '192.168.0.3');
-INSERT INTO TICKETS VALUES (9, 'Le VGA c est dépassé', 'Le VGA c est dépassé lol', 'G23', 'open', 3, '2023-10-22', 'user1', '192.168.0.1');
-INSERT INTO TICKETS VALUES (10, 'Le HDMI marche pas', 'Le HDMI marche pas lol', 'G21', 'in_progress', 2, '2022-10-20', 'user3', '192.168.0.3');
-INSERT INTO TICKETS VALUES (11, 'Linux bug', 'Linux bug lol', 'G22', 'closed', 4, '2023-10-06', 'user1', '192.168.0.2');
-INSERT INTO TICKETS VALUES (12, 'Débian probleme', 'Débian probleme lol', 'G24', 'open', 1, '2021-10-20', 'user2', '192.168.0.3');
-INSERT INTO TICKETS VALUES (13, 'Windows trop bien', 'Windows trop bien lol', 'G26', 'open', 3, '2022-04-20', 'user2', '192.168.0.2');
-INSERT INTO TICKETS VALUES (14, 'Port SSH cassé', 'Port SSH cassé lol', 'I21', 'closed', 3, '2023-03-16', 'user1', '192.168.0.1');
-INSERT INTO TICKETS VALUES (15, 'Il fait froid', 'Il fait froid lol', 'G26', 'open', 2, '2021-01-04', 'user3', '192.168.0.3');
+INSERT INTO TICKETS VALUES (3, 'Les PC sont lents', 'Les PC sont lents lol', 'I21', 'in_progress', 1, '2023-05-20', 'user2', '192.168.0.3');
+INSERT INTO TICKETS VALUES (4, 'La souris marche pas', 'La souris marche pas lol', 'G22', 'in_progress', 2, '2023-08-23', 'user2', '192.168.0.4');
+INSERT INTO TICKETS VALUES (5, 'Il manque des touches', 'Il manque des touche lol', 'G26', 'open', 1, '2023-10-09', 'user1', '192.168.0.5');
+INSERT INTO TICKETS VALUES (6, 'Le projecteur marche pas', 'Le projecteur marche pas lol', 'G25', 'closed', 2, '2022-05-20', 'user3', '192.168.0.6');
+INSERT INTO TICKETS VALUES (7, 'L ecran est pété', 'L ecran est pété lol', 'G23', 'in_progress', 4, '2023-12-15', 'user2', '192.168.0.7');
+INSERT INTO TICKETS VALUES (8, 'J ai plus d idees', 'J ai plus d idees lol', 'G24', 'open', 4, '2023-10-20', 'user1', '192.168.0.8');
+INSERT INTO TICKETS VALUES (9, 'Le VGA c est dépassé', 'Le VGA c est dépassé lol', 'G23', 'open', 3, '2023-10-22', 'user1', '192.168.0.9');
+INSERT INTO TICKETS VALUES (10, 'Le HDMI marche pas', 'Le HDMI marche pas lol', 'G21', 'in_progress', 2, '2022-10-20', 'user3', '192.168.0.10');
+INSERT INTO TICKETS VALUES (11, 'Linux bug', 'Linux bug lol', 'G22', 'closed', 4, '2023-10-06', 'user1', '192.168.0.11');
+INSERT INTO TICKETS VALUES (12, 'Débian probleme', 'Débian probleme lol', 'G24', 'open', 1, '2021-10-20', 'user2', '192.168.0.12');
+INSERT INTO TICKETS VALUES (13, 'Windows trop bien', 'Windows trop bien lol', 'G26', 'open', 3, '2022-04-20', 'user2', '192.168.0.13');
+INSERT INTO TICKETS VALUES (14, 'Port SSH cassé', 'Port SSH cassé lol', 'I21', 'closed', 3, '2023-03-16', 'user1', '192.168.0.14');
+INSERT INTO TICKETS VALUES (15, 'Il fait froid', 'Il fait froid lol', 'G26', 'open', 2, '2021-01-04', 'user3', '192.168.0.15');
 
 
 INSERT INTO Connections VALUES (1, '172.0.0.1', 'admin', 'd033e22ae348aeb5660fc2140aec35850c4da997', true, '2000-01-01 00:00:00');
