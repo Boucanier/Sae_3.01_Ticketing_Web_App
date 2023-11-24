5922ea871bedab183e06f2d508ce976241b479cc
Matis Rodier <22203556@dinfo.iut-velizy.uvsq.fr>
2023-11-24 13:22:15 +0100

    Ajout des deux actions à réaliser

--- src/pages/action_ticket.php
+++ src/pages/action_ticket.php
@@ -1,3 +1,8 @@
 <?php
+    if (isset($_GET["take"])){
+        // TODO : ajouter le technicien (SESSION) dans les interventions avec le ticket en question
+    }
 
-?>
+    elseif (isset($_GET["close"])){
+        // TODO : update du statut (closed)
+    }
