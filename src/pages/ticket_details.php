7097c62d649099c47eaa7be31ebdef0aea6d1f6f
boucanier <chironjules.78@gmail.com>
2023-12-07 21:20:18 +0100

    Un technicien ne peut clore un ticket que s'il lui est attribué

--- src/pages/ticket_details.php
+++ src/pages/ticket_details.php
@@ -31,6 +31,7 @@
     $stmt->execute();
     $stmt->bind_result($creation_date, $description, $title, $room, $emergency, $status);
     $stmt->fetch();
+    $stmt->close();
 
     echo '<div id="part_top">
         <h2>Ticket du '.$creation_date.'</h2>
@@ -63,14 +64,25 @@
             <div class="resetSubmitButtons">
                 <input type="button" value="Annuler" class="reset_buttons" onclick="history.back();">';
 
-                    # TODO: Faire une requête pour vérifier l'état du ticket afin de vérifier que la fonction est la bonne
-                    if (isset($_GET['function']) && !empty($_GET['function'])){
-                        if ($_GET['function'] == 'take'){
+                    if (isset($_GET['function']) && !empty($_GET['function']) && $_SESSION['role'] == 'tech'){
+                        if (($_GET['function'] == 'take') && ($status == 'open')){
                             echo '<input type="submit" value="Prendre en charge" name="take" class="submit_buttons">';
                         }
                         
-                        else if ($_GET['function'] == 'close'){
-                            echo '<input type="submit" value="Clore" name="close" class="submit_buttons">';
+                        else if (($_GET['function'] == 'close') && ($status == 'in_progress')){
+                            $stmt = $mysqli->prepare("SELECT tech_login FROM Interventions WHERE ticket_id = ?");
+                            $stmt->bind_param("i", $ticket_id);
+                            $stmt->execute();
+                            $stmt->bind_result($user_tech);
+                            $stmt->fetch();
+                            $stmt->close();
+
+                            if ($user_tech == $_SESSION['login']){
+                                echo '<input type="submit" value="Clore" name="close" class="submit_buttons">';
+                            }
+                            else {
+                                echo '<input type="submit" value="Clore" name="close" class="submit_buttons" disabled>';
+                            }
                         }
                         echo '<input name="ticket_id" type="hidden" value="'.$ticket_id.'"/>';
                     }
@@ -78,9 +90,6 @@
             </div>
         </div>
     </form>
-    <?php
-        $stmt->close();
-    ?>
 </main>
 <?php
     include "footer.php";
