7f86b3adc9200f8012fc48819432a9851fa327e0
Thomas GODINEAU <thomasgodineau2004@gmail.com>
2023-12-11 16:43:39 +0100

    Page de contact en anglais

--- src/pages/contact.php
+++ src/pages/contact.php
@@ -8,15 +8,25 @@
 <body>
 <?php
     include "header.php";
+    $lang = 'en';
+    $mail = array('fr' => 'Si vous souhaitez nous contacter, cliquez sur ce <a href="mailto:?to=contact.golemcorp@gmail.com" target="_blank" class="link_on_page">lien</a>.',
+        'en' => 'If you want to contact us, please click on this <a href="mailto:?to=contact.golemcorp@gmail.com" target="_blank" class="link_on_page">link</a>.');
+
+    $project = array('fr' => 'Voici notre <a href="https://github.com/Boucanier/Sae_3.01_Ticketing_Web_App" target="_blank" class="link_on_page">lien GitHub</a> pour ce projet.',
+        'en' => 'Here is our <a href="https://github.com/Boucanier/Sae_3.01_Ticketing_Web_App" target="_blank" class="link_on_page">Github link</a> for this project');
 ?>
 <main>
     <div id="contact_containers">
         <div class="contact_text">
-            Si vous souhaitez nous contacter, cliquez sur ce <a href="mailto:?to=contact.golemcorp@gmail.com" target="_blank" class="link_on_page">lien</a>.
+            <?php
+                echo $mail[$lang];
+            ?>
         </div>
         <br>
         <div class="contact_text">
-            Voici notre <a href="https://github.com/Boucanier/Sae_3.01_Ticketing_Web_App" target="_blank" class="link_on_page">lien GitHub</a> pour ce projet.
+            <?php
+                echo $project[$lang];
+            ?>
         </div>
     </div>
 </main>
