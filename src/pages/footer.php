f2bc42227659edc53870eeae73a936f6ef884786
Thomas GODINEAU <thomasgodineau2004@gmail.com>
2023-12-11 16:55:41 +0100

    Ajout de l'anglais à footer.php

--- src/pages/footer.php
+++ src/pages/footer.php
@@ -1,12 +1,25 @@
 <footer>
-    <a href="contact.php">Nous contacter</a>
-    <a href="">Changer la langue</a>
-    <?php 
+    <?php
+        $lang = 'en';
+
+        $contact = array('fr' => '<a href="contact.php">Nous contacter</a>',
+            'en' => '<a href="contact.php">Contact us</a>');
+
+        $language = array('fr' => ' <a href="">Change language</a>',
+            'en' => '<a href="">Changer la langue</a>');
+
+        echo $contact[$lang];
+        echo $language[$lang];
+
         if (isset($_SESSION['font']) && $_SESSION['font'] == "dyslexic"){
-            echo '<a href="font_switch.php?font=normal">Passer en police normale</a>';
+            $dys_on = array('fr' => '<a href="font_switch.php?font=normal">Passer en police normale</a>',
+                'en' => '<a href="font_switch.php?font=normal">Change to normal font</a>');
+            echo $dys_on[$lang];
         }
         else {
-            echo '<a href="font_switch.php?font=dyslexic">Passer en police dyslexie</a>';
+            $dys_off = array('fr' => '<a href="font_switch.php?font=dyslexic">Passer en police dyslexie</a>',
+                'en' => '<a href="font_switch.php?font=dyslexic">Change to dyslexic font</a>');
+            echo $dys_off[$lang];
         }
     ?>
 </footer>
