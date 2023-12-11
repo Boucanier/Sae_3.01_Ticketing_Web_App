a5e11cf995eceecd3fecec30707f292a2f0bb82a
boucanier <chironjules.78@gmail.com>
2023-12-11 22:24:49 +0100

    optimisation du fichier

--- src/pages/footer_switch.php
+++ src/pages/footer_switch.php
@@ -1,21 +1,16 @@
 <?php
     session_start();
-    if (isset($_GET['font']) && $_GET['font'] == "dyslexic"){
-        $_SESSION['font'] = "dyslexic";
+    
+    if (isset($_GET['font']) && in_array($_GET['font'], array('normal', 'dyslexic'))){
+        $_SESSION['font'] = $_GET['font'];
         header("Location: ".$_SERVER['HTTP_REFERER']);
-    }
-    else if (isset($_GET['font']) && $_GET['font'] == "normal"){
-        $_SESSION['font'] = "normal";
-        header("Location: ".$_SERVER['HTTP_REFERER']);
-    }
-    else {
-        header("Location: index.php");
     }
 
-    if (isset($_GET['lang']) && in_array($_GET['lang'], array('fr', 'en'))){
+    else if (isset($_GET['lang']) && in_array($_GET['lang'], array('fr', 'en'))){
         $_SESSION['lang'] = $_GET['lang'];
         header("Location: ".$_SERVER['HTTP_REFERER']);
     }
+
     else {
         header("Location: index.php");
     }
