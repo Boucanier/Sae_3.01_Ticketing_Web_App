456a14b35cedd7f4f23c93fdb2fcd51ae68bd120
Thomas Godineau <thomasgodineau2004@gmail.com>
2024-01-10 21:35:44 +0100

    Ajout commentaires dans le code

--- src/R/app.R
+++ src/R/app.R
@@ -1,3 +1,4 @@
+# Chargement de la librarie shiny
 library(shiny)
 
 # On utilise le port 3000
@@ -9,10 +10,8 @@ ui = fluidPage(
 	#Titre de la page
 	titlePanel("Statistiques"),
 
-	# Sidebar
 	sidebarLayout(
 
-		# Sidebar panel
 		sidebarPanel(
 
 			titlePanel("Pourcentage de tickets selon leur status"),
@@ -31,7 +30,18 @@ ui = fluidPage(
 				max = 40,
 				min = 1,
                 value = 15
-            ),
+            )
+		),
+
+		mainPanel(
+		    verbatimTextOutput('texte'),
+			plotOutput('graph1')
+		)
+	),
+
+	sidebarLayout(
+
+	    sidebarPanel(
 
             titlePanel("Pourcentage de connexion échouées"),
 
@@ -43,15 +53,12 @@ ui = fluidPage(
 				min = 1,
                 value = 15
             )
-		),
+	    ),
 
-		# Main panel
-		mainPanel(
-		    verbatimTextOutput('texte'),
-			plotOutput('graph1'),
+	    mainPanel(
 			plotOutput('graph2'),
 			plotOutput('graph3')
-		)
+	    )
 	)
 )
 
@@ -60,15 +67,23 @@ ui = fluidPage(
 # Serveur
 server = function(input, output) {
 
+    # On récupère les données des fichiers CSV
 	tickets = read.csv("tickets.csv")
 	connexions = read.csv("connexions.csv")
 
+    # On affiche les résultats
+
+    # Premier affichage
 	output$texte = renderPrint(
 	    {
+	        # On récupère les valeurs entrées
             type = input$statusType
             nbTickets = input$nbTickets
+
+            # On calcule le pourcentage
             pourcentage = (length(which((tickets$status)[0:nbTickets] == type)))/nbTickets
 
+            # On affiche le résultats
             if (type == "open"){
                 print("Pourcentage de ticket ouvert : ")
                 print(pourcentage)
@@ -84,11 +99,16 @@ server = function(input, output) {
 	    }
 	)
 
+    # Deuxième affichage
 	output$graph1 = renderPlot(
 	    {
+	        # On récupère les valeurs entrées
             nbTickets = input$nbTickets
+
+            # On calcule le pourcentage
             pourcentages = data.frame("status" = c("open", "in_progress", "closed"), "pourcentage" = c(round(((length(which((tickets$status)[0:nbTickets] == 'open')))/nbTickets), 2), round(((length(which((tickets$status)[0:nbTickets] == 'in_progress')))/nbTickets), 2), round(((length(which((tickets$status)[0:nbTickets] == 'closed')))/nbTickets), 2)))
 
+            # On affiche le résultats
             pie(
                 pourcentages$pourcentage,
                 col = c("#AAFFAA", "#FFEE44", "#FFAAAA"),
@@ -105,11 +125,16 @@ server = function(input, output) {
 		}
 	)
 
+    # Troisième affichage
 	output$graph2 = renderPlot(
 	    {
+	        # On récupère les valeurs entrées
 	        nbCo = input$nbCo
+
+            # On calcule le pourcentage
 	        succes = data.frame( "status" = c("réussie", "échouée"), "succes" = c(round(((length(which((connexions$succes)[0:nbCo] == 'true')))/nbCo), 2), round(((length(which((connexions$succes)[0:nbCo] == 'false')))/nbCo), 2)))
 
+            # On affiche le résultats
 	        pie(
                 succes$succes,
                 col = c("#AAFFAA", "#FFAAAA"),
@@ -126,9 +151,13 @@ server = function(input, output) {
 	    }
 	)
 
+    # Quatrième affichage
 	output$graph3 = renderPlot(
         {
+	        # On récupère les valeurs entrées
             nbCo = input$nbCo
+
+            # On calcule le pourcentage
             pourcentages = c()
 
 	        for (i in 1:nbCo){
@@ -136,6 +165,7 @@ server = function(input, output) {
 	            pourcentages = c(pourcentages, pourcentage)
 	        }
 
+            # On affiche le résultats
             plot(seq(1, nbCo), pourcentages, pch=4, xlab = "Pourcentage de connexions réussies", ylab = "Nombre de tentatives de connexions")
             title("Pourcentage de connexions réussies")
         }
