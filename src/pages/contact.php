b4ffa3c2cf14ed558dff7c13caeedc0c393580e2
boucanier <chironjules.78@gmail.com>
2024-02-04 16:48:11 +0100

    Titre de la page

--- src/pages/contact.php
+++ src/pages/contact.php
@@ -7,8 +7,11 @@ $mail = array('fr' => '<p>Si vous souhaitez nous contacter, cliquez sur <a href=
 
 $project = array('fr' => '<p>Voici notre <a href="https://github.com/Boucanier/Sae_3.01_Ticketing_Web_App" target="_blank" class="link_on_page">lien GitHub</a> pour ce projet</p>',
     'en' => '<p>Here is our <a href="https://github.com/Boucanier/Sae_3.01_Ticketing_Web_App" target="_blank" class="link_on_page">Github link</a> for this project</p>');
+
+$infoTop = array('fr' => 'Nous contacter', 'en' => 'Contact us');
+
+echo '<main><div id="part_top"><h2>'.$infoTop[$lang].'</h2></div>';
 ?>
-<main>
     <div id="contact_containers">
         <div class="contact_text">
             <?php
