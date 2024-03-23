550d772b3076bd6ba0ac3ec24546436a7a540ad5
boucanier <chironjules.78@gmail.com>
2024-03-23 15:16:40 +0100

    Ajout du succès de la connexion dans les logs

--- src/logs_job.sh
+++ src/logs_job.sh
@@ -18,7 +18,7 @@ ticketStmt="SELECT CONCAT_WS(',', DATE_FORMAT(creation_date,'%d/%m/%Y'), ticket_
     ORDER BY creation_date DESC;"
 
     # On récupère toutes les connexions
-connStmt="SELECT CONCAT_WS(',', DATE_FORMAT(date_co,'%d/%m/%Y %T'), id_co, login, password, ip_address) 'date,id,login,password,ip_address'
+connStmt="SELECT CONCAT_WS(',', DATE_FORMAT(date_co,'%d/%m/%Y %T'), id_co, login, password, ip_address, succes) 'date,id,login,password,ip_address,success'
     FROM Connections
     WHERE DATE(date_co) = CURDATE() - INTERVAL 1 DAY
     ORDER BY date_co DESC;"
